2953e4a68d7de76275658341f6efd2feb7e0f7e5
Romina Gutierrez <60100142+rogutier@users.noreply.github.com>
2023-03-31 22:43:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function saludo() {
+  alert("Bienvenido");
+  var nombre = prompt("Ingrese su nombre: ");
+  alert("Hola " + nombre);
+}
+
+var inicio = document.getElementsByClassName("link")[0];
+
+inicio.addEventListener("click", function() {
+  saludo();
+});
+
+function despedida() {
+  alert("Gracias por visitar mi página.");
+}
+
+var acerca = document.getElementsByClassName("link")[1];
+
+acerca.addEventListener("click", function() {
+  despedida();
+});
+
+function regresa() {
+  alert("Regresa cuando quieras.");
+}
+
+var contacto = document.getElementsByClassName("link")[2];
+
+contacto.addEventListener("click", function() {
+  regresa();
+});
